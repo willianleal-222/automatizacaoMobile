d970b31eae19dc7b6d84a4bfc8daf981ef4782b8
Willian Leal <willianleal@hotmail.com>
2025-09-03 08:42:22 -0300

    commit

--- src/test/java/ExemplosAutomacaoAppium.java
+++ src/test/java/ExemplosAutomacaoAppium.java
@@ -87,13 +87,16 @@ public class ExemplosAutomacaoAppium {
 
     @Test
     public void recorded(){
+        WebElement el1 = driver.findElement(AppiumBy.accessibilityId("Views"));
+        el1.click();
 
-        WebElement el2 = driver.findElement(AppiumBy.accessibilityId("Views"));
+        WebElement el2 = driver.findElement(AppiumBy.accessibilityId("Drag and Drop"));
         el2.click();
-        WebElement el3 = driver.findElement(AppiumBy.accessibilityId("Drag and Drop"));
+
+        WebElement el3 = driver.findElement(AppiumBy.id("io.appium.android.apis:id/drag_dot_1"));
         el3.click();
-        WebElement el4 = driver.findElement(AppiumBy.id("io.appium.android.apis:id/drag_dot_1"));
-        el4.click();
+
+
 
 
     }
